86f0c3c82bc876269b1024321efe10ab2477252e
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-01-19 23:37:14 -0600

    Almost complete

--- leaflet-step1/static/js/logic.js
+++ leaflet-step1/static/js/logic.js
@@ -1,37 +1,48 @@
-var myMap = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 11
-  });
+
+// url for earthquakedata
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
   
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
+
+// Grab data with d3
+d3.json(geoData, function(data) {
+  //console.log(data);
+  createFeatures(data.features);
+});
+//function to create features for data
+function createFeatures(earthData) {
+    function onEachfeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.title + "</h3>");
+    }
+    //GeoJson layer
+    var earthquakes = L.geoJSON(earthData, {
+        onEachfeature: onEachfeature
+    });
+    createMap(earthquakes);
+    }
+function createMap(earthquakes) {
+    // Adding tile layer
+  var backgroundmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>", 
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/lights-v10",
+    id: "mapbox/light-v10",
     accessToken: API_KEY
-  }).addTo(myMap);
-
-  
-  // Load in geojson data
-  var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-  
-  var geojson;
-  
-  // Grab data with d3
-  d3.json(geoData, function(data) {
-    console.log(data);
-    
-      for (var i = 0; i < data.features.length; i++) {
-        
-        // Set the data location property to a variable
-        var features = data.features[i];
-        var geometry = features.geometry;
-        var coordinates = geometry.coordinates;
-        var xlong = coordinates[0];
-        var ylong = coordinates[1];
-        var zlong = coordinates[2];
-        console.log(xlong)
-      }; 
-    })  
+  });
+//map to hold layers
+  var baseMaps = { "Light Map": backgroundmap
+    };
+//add earthquakes
+    var overlayMap = {
+    "Earthquakes": earthquakes
+    };
+//create map
+    var myMap = L.map("map", {
+    center: [15, -30],
+    zoom: 3,
+    layers: [backgroundmap, earthquakes]
+  });
+//layer control 
+    L.control.layers(baseMaps, overlayMap, {
+    collapsed: false
+    }).addTo(myMap);
+}
